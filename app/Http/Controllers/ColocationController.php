2311ccdb0654fbaba74b560687dbd26fd9ef53c4
moijxbwlfjod-design <moijxbwlfjod@gmail.com>
2026-02-25 12:23:21 +0000

    fix: check if the user already had a colocation

--- app/Http/Controllers/ColocationController.php
+++ app/Http/Controllers/ColocationController.php
@@ -6,10 +6,13 @@ use Illuminate\Http\Request;
 use App\Http\Requests\ColocationRequest;
 use App\MyFunctions\SlugGenerator;
 use App\Models\Colocation;
+use App\Models\ColocationRole;
+use Illuminate\Support\Facades\Auth;
 
 class ColocationController extends Controller
 {
     public function create(ColocationRequest $request){
+        if (Auth::User()->colocation_role->name == 'owner') return back()->withErrors(['error' => 'As owner you can\'t create a new colocation']);
         $validation = $request->validate();
         $slug = SlugGenerator::UniqueSlug(Colocation, $request->title);
         $request->file('image')->move(public_path('colocations_images'), $slug);
@@ -21,6 +24,7 @@ class ColocationController extends Controller
             'owner_id' => $request->owner_id,
             'status' => $request->status,
         ]);
+        Auth::User()->colocation_role_id = ColocationRole::where('name', 'owner')->value('id');
         return back()->with(['msg' => 'Store created successfully']);
     }
 }
