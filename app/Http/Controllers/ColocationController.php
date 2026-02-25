2a5e7e7d7ceda4381bde3403bfe7367a4b152b35
moijxbwlfjod-design <moijxbwlfjod@gmail.com>
2026-02-25 12:06:29 +0000

    feat: finish store creation fonctionality

--- app/Http/Controllers/ColocationController.php
+++ app/Http/Controllers/ColocationController.php
@@ -3,10 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\ColocationRequest;
+use App\MyFunctions\SlugGenerator;
+use App\Models\Colocation;
 
 class ColocationController extends Controller
 {
-    public function create(Request $request){
-        
+    public function create(ColocationRequest $request){
+        $validation = $request->validate();
+        $slug = SlugGenerator::UniqueSlug(Colocation, $request->title);
+        $request->file('image')->move(public_path('colocations_images'), $slug);
+        Colocation::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'image_path' => public_path("/colocations/{$slug}"),
+            'house_id' => $request->house_id,
+            'owner_id' => $request->owner_id,
+            'status' => $request->status,
+        ]);
+        return back()->with(['msg' => 'Store created successfully']);
     }
 }
