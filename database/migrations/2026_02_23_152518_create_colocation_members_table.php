8502135353266b00438d7fe2ed61891b8edd66f6
moijxbwlfjod-design <moijxbwlfjod@gmail.com>
2026-02-27 15:34:41 +0000

    fix: fix the migration file of colocation_members and add colocation_role and lefted_at columns

--- database/migrations/2026_02_23_152518_create_colocation_members_table.php
+++ database/migrations/2026_02_23_152518_create_colocation_members_table.php
@@ -14,7 +14,9 @@ return new class extends Migration
         Schema::create('colocation_members', function (Blueprint $table) {
             $table->id();
             $table->foreignId('colocation_id')->constrained()->onDelete('cascade');
-            $table->foreignId('member_id')->constrained('users', 'id')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('colocation_role_id')->constrained()->onDelete('cascade');
+            $table->timestamp('lefted_at')->nullable();
             $table->timestamps();
         });
     }
