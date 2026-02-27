44f596f2582452af98d19c5e9e8e01eabf9bde60
moijxbwlfjod-design <moijxbwlfjod@gmail.com>
2026-02-27 11:15:25 +0000

    fix: fix the colocation and the colocation member controller

--- app/Http/Controllers/ColocationController.php
+++ app/Http/Controllers/ColocationController.php
@@ -5,9 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\ColocationRequest;
 use App\MyFunctions\SlugGenerator;
-use App\Models\Colocation;
 use App\Models\ColocationRole;
 use Illuminate\Support\Facades\Auth;
+use App\Models\ColocationMember;
+use App\Models\Colocation;
+
 
 class ColocationController extends Controller
 {
@@ -17,14 +19,14 @@ class ColocationController extends Controller
     }
 
     public function create(ColocationRequest $request){
-        //dd(Auth::user()->colocation_role_id);
         $user = Auth::User();
         if ($user->colocation_role_id == ColocationRole::where('name', 'owner')->value('id')) return back()->with(['error' => 'As owner you can\'t create a new colocation']);
         $validation = $request->validated();
-        $colocation = new Colocation();
-        $slug = SlugGenerator::UniqueSlug($colocation, $request->title);
+        //$colocation = new Colocation();
+        //dd(Colocation::class);
+        $slug = SlugGenerator::UniqueSlug(new Colocation(), $request->title);
         $request->file('image')->move(public_path('colocations_images'), "{$slug}.png");
-        $colocation::create([
+        $colocation = Colocation::create([
             'title' => $request->title,
             'description' => $request->description,
             'image_path' => "/colocations_images/{$slug}.png",
@@ -33,6 +35,7 @@ class ColocationController extends Controller
             'owner_id' => Auth::User()->id,
             'status' => 'active',
         ]);
+        ColocationMember::create(['member_id' => $user->id, 'colocation_id' => $colocation->id]);
         $user->colocation_role_id = ColocationRole::where('name', 'owner')->value('id');
         $user->save();
         return back()->with('msg', 'Store created successfully');
--- app/Http/Controllers/ColocationMemberController.php
+++ app/Http/Controllers/ColocationMemberController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\ColocationMemberRequest;
+use App\Models\ColocationMember;
 
 class ColocationMemberController extends Controller
 {
-    //
+    public static function create(ColocationMemberRequest $request){
+        $validation = $request->validated();
+        ColocationMember::create([
+            'colocation_id' => $request->colocation_id,
+            'member_id' => $request->member_id
+        ]);
+    }
 }
