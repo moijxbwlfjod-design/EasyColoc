c79e7a1b9e61d204480dd0c58c108e3df79a6ac9
moijxbwlfjod-design <moijxbwlfjod@gmail.com>
2026-03-02 14:03:37 +0000

    fix: add the colocation_id column to categories table

--- database/migrations/2026_02_23_150918_create_categories_table.php
+++ database/migrations/2026_02_23_150918_create_categories_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->foreignId('colocation_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
